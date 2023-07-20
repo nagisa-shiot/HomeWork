62d2a402c308ecf66915fde89c6448c6d9817d0d
Данил Щипцов <bondar-seryy@mail.ru>
2023-07-20 23:23:50 +0800

    Наибольшее из 3 чисел

---
+++ Task004/Program.cs
@@ -0,0 +1,12 @@
+﻿ int a = 10;
+ int b = 19;
+ int c = 12;
+ 
+ int max = a;
+
+ if (a > max ) max = a;
+ if (b > max ) max = b;
+ if (c > max ) max = c;
+
+ Console.Write("max = ");
+ Console.WriteLine(max);
