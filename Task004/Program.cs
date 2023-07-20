724bf436b3fe84fb9b8fc4e06e00379677bae223
Данил Щипцов <bondar-seryy@mail.ru>
2023-07-20 23:49:40 +0800

    Наибольшее из 3 чисел

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -9,5 +9,5 @@ int number3 = int.Parse(Console.ReadLine());
 int max = number1;
  if (number1 > max ) max = number1;
  if (number2 > max ) max = number2;
- if (number3 > max ) max = number3 ;
+ if (number3 > max ) max = number3;
  Console.WriteLine("Число " + max + " наибольшее");
