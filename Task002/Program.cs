1da598c5b80aa7fcc19d5a46945d6f14ca617486
Данил Щипцов <bondar-seryy@mail.ru>
2023-07-20 23:14:53 +0800

    Наибольшее и наименьшее

---
+++ Task002/Program.cs
@@ -0,0 +1,15 @@
+﻿ int a = 10;
+ int b = 9;
+ int max = a;
+ int min = b;
+ if (a > max ) max = a;
+ if (b > max ) max = b;
+
+ if (a < min ) min = a;
+ if (b < min ) min = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min= ");
+Console.WriteLine(min);
