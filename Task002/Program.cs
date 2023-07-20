8e977b7886fb8eed702b8fe5c8fbaea718a7fa72
Данил Щипцов <bondar-seryy@mail.ru>
2023-07-20 23:37:51 +0800

    Наибольшее и наименьшее

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,15 +1,20 @@
-﻿ int a = 10;
- int b = 9;
- int max = a;
- int min = b;
- if (a > max ) max = a;
- if (b > max ) max = b;
-
- if (a < min ) min = a;
- if (b < min ) min = b;
-
-Console.Write("max = ");
-Console.WriteLine(max);
-
-Console.Write("min= ");
-Console.WriteLine(min);
+﻿﻿Console.WriteLine("Введите 2 числа,");
+Console.WriteLine("чтобы узнать какое большее, а какое меньшее.");
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine("Число " + number1 + " большее");
+    Console.Write("Число " + number2 + " меньшее");
+}
+else if (number1 == number2)
+{
+    Console.WriteLine("Число " + number1 + " равно числу " + number2);
+}
+else
+{
+    Console.WriteLine("Число " + number2 + " большее");
+    Console.Write("Число " + number1 + " меньшее");
+}
