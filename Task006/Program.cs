526ce2bd6c78923d8694d045e4f46c1cb244160e
Данил Щипцов <bondar-seryy@mail.ru>
2023-07-20 23:57:37 +0800

    Чётное или нет

---
+++ Task006/Program.cs
@@ -0,0 +1,13 @@
+﻿﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int remain = number % 2;
+if (remain == 0)
+{
+    Console.WriteLine("Число " + number+ " четное?");
+    Console.WriteLine("Да!");
+}
+else
+{
+    Console.WriteLine("Число " + number + " четное?");
+    Console.WriteLine("Нет!");
+}
