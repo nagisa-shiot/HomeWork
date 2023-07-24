1ecf140173f6f5650464dce9d509b19b1f10376d
Данил Щипцов <bondar-seryy@mail.ru>
2023-07-24 22:31:51 +0800

    Показывает 2 цифру в 3 значном числ

---
+++ Task010/Program.cs
@@ -0,0 +1,16 @@
+﻿int number = ReadInt("Введите трехзначное число: ");
+int amount = number.ToString().Length;
+
+Console.WriteLine(InCenter(number));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int InCenter(int a)
+{
+    
+    int result = ((a / 10) % 10);
+    return result;
+}
