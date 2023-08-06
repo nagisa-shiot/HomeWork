eceb050547a46e2f63bb4cc1a0f7366beffe5d99
nagisa-shiot <109577505+nagisa-shiot@users.noreply.github.com>
2023-08-06 12:32:25 +0800

    Add files via upload

---
+++ Task029/Program.cs
@@ -0,0 +1,28 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 8);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        position++;
+    }
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
+Console.Write($"[{string.Join(",",array)}]");
